b4afbbe034f11747eaa1d0b80943c8ca5063e1e6
Hisao <jaimehisao2011@gmail.com>
2021-02-17 01:01:50 -0600

    Done

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from retriever import get_catalogue
 from retriever import get_records
 from processor import process
 
-schedule.every().hour.at(":44").do(get_records)
-schedule.every().hour.at(":45").do(get_catalogue)
-schedule.every().hour.at(":46").do(process)
+schedule.every().day.at("21:01").do(get_records)
+schedule.every().day.at("21:02").do(get_catalogue)
+schedule.every().day.at("21:03").do(process)
 
 print('MX-COVID bot alive and running!')
 while True:
